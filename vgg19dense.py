975236f7b3ccd69b9066c7fa4707673a7f2a05b1
zo7 <zo7.flynn@gmail.com>
2016-10-30 13:41:27 -0400

    Modify VGG19 to output feature maps

--- vgg19dense.py
+++ vgg19dense.py
@@ -13,7 +13,7 @@ import warnings
 
 from keras.models import Model
 from keras.layers import Flatten, Dense, Input
-from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import AtrousConv2D, Convolution2D, MaxPooling2D
 from keras.preprocessing import image
 from keras.utils.layer_utils import convert_all_kernels_in_model
 from keras.utils.data_utils import get_file
@@ -27,8 +27,7 @@ TH_WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/relea
 TF_WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5'
 
 
-def VGG19(include_top=True, weights='imagenet',
-          input_tensor=None):
+def VGG19Dense(weights='imagenet', input_tensor=None):
     '''Instantiate the VGG19 architecture,
     optionally loading weights pre-trained
     on ImageNet. Note that when using TensorFlow,
@@ -42,8 +41,6 @@ def VGG19(include_top=True, weights='imagenet',
     specified in your Keras config file.
 
     # Arguments
-        include_top: whether to include the 3 fully-connected
-            layers at the top of the network.
         weights: one of `None` (random initialization)
             or "imagenet" (pre-training on ImageNet).
         input_tensor: optional Keras tensor (i.e. output of `layers.Input()`)
@@ -58,15 +55,9 @@ def VGG19(include_top=True, weights='imagenet',
                          '(pre-training on ImageNet).')
     # Determine proper input shape
     if K.image_dim_ordering() == 'th':
-        if include_top:
-            input_shape = (3, 224, 224)
-        else:
-            input_shape = (3, None, None)
+        input_shape = (3, None, None)
     else:
-        if include_top:
-            input_shape = (224, 224, 3)
-        else:
-            input_shape = (None, None, 3)
+        input_shape = (None, None, 3)
 
     if input_tensor is None:
         img_input = Input(shape=input_shape)
@@ -75,43 +66,37 @@ def VGG19(include_top=True, weights='imagenet',
             img_input = Input(tensor=input_tensor)
         else:
             img_input = input_tensor
+
     # Block 1
-    x = Convolution2D(64, 3, 3, activation='relu', border_mode='same', name='block1_conv1')(img_input)
-    x = Convolution2D(64, 3, 3, activation='relu', border_mode='same', name='block1_conv2')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+    x = AtrousConv2D(64, 3, 3, atrous_rate=(1,1), activation='relu', border_mode='same', name='block1_conv1')(img_input)
+    x = AtrousConv2D(64, 3, 3, atrous_rate=(1,1), activation='relu', border_mode='same', name='block1_conv2')(x)
+    #x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
 
     # Block 2
-    x = Convolution2D(128, 3, 3, activation='relu', border_mode='same', name='block2_conv1')(x)
-    x = Convolution2D(128, 3, 3, activation='relu', border_mode='same', name='block2_conv2')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+    x = AtrousConv2D(128, 3, 3, atrous_rate=(2,2), activation='relu', border_mode='same', name='block2_conv1')(x)
+    x = AtrousConv2D(128, 3, 3, atrous_rate=(2,2), activation='relu', border_mode='same', name='block2_conv2')(x)
+    #x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
 
     # Block 3
-    x = Convolution2D(256, 3, 3, activation='relu', border_mode='same', name='block3_conv1')(x)
-    x = Convolution2D(256, 3, 3, activation='relu', border_mode='same', name='block3_conv2')(x)
-    x = Convolution2D(256, 3, 3, activation='relu', border_mode='same', name='block3_conv3')(x)
-    x = Convolution2D(256, 3, 3, activation='relu', border_mode='same', name='block3_conv4')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+    x = AtrousConv2D(256, 3, 3, atrous_rate=(4,4), activation='relu', border_mode='same', name='block3_conv1')(x)
+    x = AtrousConv2D(256, 3, 3, atrous_rate=(4,4), activation='relu', border_mode='same', name='block3_conv2')(x)
+    x = AtrousConv2D(256, 3, 3, atrous_rate=(4,4), activation='relu', border_mode='same', name='block3_conv3')(x)
+    x = AtrousConv2D(256, 3, 3, atrous_rate=(4,4), activation='relu', border_mode='same', name='block3_conv4')(x)
+    #x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
 
     # Block 4
-    x = Convolution2D(512, 3, 3, activation='relu', border_mode='same', name='block4_conv1')(x)
-    x = Convolution2D(512, 3, 3, activation='relu', border_mode='same', name='block4_conv2')(x)
-    x = Convolution2D(512, 3, 3, activation='relu', border_mode='same', name='block4_conv3')(x)
-    x = Convolution2D(512, 3, 3, activation='relu', border_mode='same', name='block4_conv4')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+    x = AtrousConv2D(512, 3, 3, atrous_rate=(8,8), activation='relu', border_mode='same', name='block4_conv1')(x)
+    x = AtrousConv2D(512, 3, 3, atrous_rate=(8,8), activation='relu', border_mode='same', name='block4_conv2')(x)
+    x = AtrousConv2D(512, 3, 3, atrous_rate=(8,8), activation='relu', border_mode='same', name='block4_conv3')(x)
+    x = AtrousConv2D(512, 3, 3, atrous_rate=(8,8), activation='relu', border_mode='same', name='block4_conv4')(x)
+    #x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
 
     # Block 5
-    x = Convolution2D(512, 3, 3, activation='relu', border_mode='same', name='block5_conv1')(x)
-    x = Convolution2D(512, 3, 3, activation='relu', border_mode='same', name='block5_conv2')(x)
-    x = Convolution2D(512, 3, 3, activation='relu', border_mode='same', name='block5_conv3')(x)
-    x = Convolution2D(512, 3, 3, activation='relu', border_mode='same', name='block5_conv4')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
-
-    if include_top:
-        # Classification block
-        x = Flatten(name='flatten')(x)
-        x = Dense(4096, activation='relu', name='fc1')(x)
-        x = Dense(4096, activation='relu', name='fc2')(x)
-        x = Dense(1000, activation='softmax', name='predictions')(x)
+    x = AtrousConv2D(512, 3, 3, atrous_rate=(16,16), activation='relu', border_mode='same', name='block5_conv1')(x)
+    x = AtrousConv2D(512, 3, 3, atrous_rate=(16,16), activation='relu', border_mode='same', name='block5_conv2')(x)
+    x = AtrousConv2D(512, 3, 3, atrous_rate=(16,16), activation='relu', border_mode='same', name='block5_conv3')(x)
+    x = AtrousConv2D(512, 3, 3, atrous_rate=(16,16), activation='relu', border_mode='same', name='block5_conv4')(x)
+    #x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
 
     # Create model
     model = Model(img_input, x)
@@ -120,14 +105,9 @@ def VGG19(include_top=True, weights='imagenet',
     if weights == 'imagenet':
         print('K.image_dim_ordering:', K.image_dim_ordering())
         if K.image_dim_ordering() == 'th':
-            if include_top:
-                weights_path = get_file('vgg19_weights_th_dim_ordering_th_kernels.h5',
-                                        TH_WEIGHTS_PATH,
-                                        cache_subdir='models')
-            else:
-                weights_path = get_file('vgg19_weights_th_dim_ordering_th_kernels_notop.h5',
-                                        TH_WEIGHTS_PATH_NO_TOP,
-                                        cache_subdir='models')
+            weights_path = get_file('vgg19_weights_th_dim_ordering_th_kernels_notop.h5',
+                                    TH_WEIGHTS_PATH_NO_TOP,
+                                    cache_subdir='models')
             model.load_weights(weights_path)
             if K.backend() == 'tensorflow':
                 warnings.warn('You are using the TensorFlow backend, yet you '
@@ -140,14 +120,9 @@ def VGG19(include_top=True, weights='imagenet',
                               'at ~/.keras/keras.json.')
                 convert_all_kernels_in_model(model)
         else:
-            if include_top:
-                weights_path = get_file('vgg19_weights_tf_dim_ordering_tf_kernels.h5',
-                                        TF_WEIGHTS_PATH,
-                                        cache_subdir='models')
-            else:
-                weights_path = get_file('vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5',
-                                        TF_WEIGHTS_PATH_NO_TOP,
-                                        cache_subdir='models')
+            weights_path = get_file('vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5',
+                                    TF_WEIGHTS_PATH_NO_TOP,
+                                    cache_subdir='models')
             model.load_weights(weights_path)
             if K.backend() == 'theano':
                 convert_all_kernels_in_model(model)
@@ -155,14 +130,16 @@ def VGG19(include_top=True, weights='imagenet',
 
 
 if __name__ == '__main__':
-    model = VGG19(include_top=True, weights='imagenet')
+    model = VGG19Dense(weights='imagenet')
 
     img_path = 'cat.jpg'
-    img = image.load_img(img_path, target_size=(224, 224))
+    img = image.load_img(img_path, target_size=(512, 512))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
     print('Input image shape:', x.shape)
 
     preds = model.predict(x)
-    print('Predicted:', decode_predictions(preds))
+
+    print(preds.shape)
+
